0f8f82f71324628cc3596acc802cd46fda39e2f0
daniellpan8 <daniellpan8@gmail.com>
2024-10-31 16:14:55 -0400

    add c3 stuff

--- src/main-esp32c3.cpp
+++ src/main-esp32c3.cpp
@@ -1,14 +1,56 @@
-#include "Arduino.h"
+// //#include <Wire.h>
+// #include <LoRa.h>
+// #include <SPI.h>
 
-#define LED_PIN 8  // Pin 3 on ESP32-C3
+// // #define NSS 3
+// // #define RST 4
+// // #define DI0 2
+// // #define SCK 8
+// // #define MISO 9
+// // #define MOSI 10
+
+// #define NSS   7   // Chip Select
+// #define RST   6   // Reset
+// #define DI0   5   // Interrupt pin
+// #define SCK   4   // Serial Clock
+// #define MISO  3   // Master In Slave Out
+// #define MOSI  2
+
+// String LoRaData;
+
+// void setup() {
+//   Serial.begin(115200);
+//   LoRa.setPins(NSS, RST, DI0);
+
+//   while (!LoRa.begin(433E6)) {
+//     Serial.println(".");
+//     delay(500);
+//   }
+//   LoRa.setSyncWord(0xF1);
+//   Serial.println("LoRa Initializing Successful (Receiver)!");
+
+// }
+
+// void loop() {
+//   int packetSize = LoRa.parsePacket();
+//   if (packetSize) {
+//     Serial.print("Received packet: ");
+//     while (LoRa.available()) {
+//       LoRaData = LoRa.readString();
+//     }
+//     Serial.println(LoRaData);
+//   }
+// }
+
+#include "Arduino.h"
+#define LED_PIN 8
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);  // Set pin 3 as output
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);  // Turn on (output high)
-  delay(1000);  // Wait 1 second
-  digitalWrite(LED_PIN, LOW);   // Turn off (output low)
-  delay(1000);  // Wait 1 second
+  
+    digitalWrite(LED_PIN, HIGH);
+      // Pause between bursts
 }
