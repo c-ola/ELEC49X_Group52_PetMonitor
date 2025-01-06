7f9e5336ed121a9e0e4782206fecfefa327e01d4
Daniell Pan <daniellpan8@gmail.com>
2025-01-06 15:57:44 -0500

    hi

--- src/main-ble.cpp
+++ src/main-ble.cpp
@@ -1,122 +1,79 @@
+#include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
-#include "Arduino.h"
+#include <BLEServer.h>
+#include <BLEAdvertising.h>
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pSensorCharacteristic = NULL;
-BLECharacteristic* pLedCharacteristic = NULL;
+// Define service and characteristic UUIDs
+#define SERVICE_UUID "19b10000-e8f2-537e-4f6c-d104768a1214"
+#define CHAR_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
+#define DEVICE_NAME "ESP32_BLE"
+
+// Global variables
+float latitude = 37.7749; // Example latitude
+float longitude = -122.4194; // Example longitude
+BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint32_t value = 0;
-
-const int ledPin = 2; // Use the appropriate GPIO pin for your setup
-
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-#define SERVICE_UUID        "19b10000-e8f2-537e-4f6c-d104768a1214"
-#define SENSOR_CHARACTERISTIC_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
-#define LED_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
+BLEServer *pServer;
 
 class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+    void onConnect(BLEServer* pServer) override {
         deviceConnected = true;
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
+        Serial.println("Device connected");
     }
-};
-
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic* pLedCharacteristic) {
-        std::string value = pLedCharacteristic->getValue();
-        if (value.length() > 0) {
-            Serial.print("Characteristic event, written: ");
-            Serial.println(static_cast<int>(value[0])); // Print the integer value
-
-            int receivedValue = static_cast<int>(value[0]);
-            if (receivedValue == 1) {
-                digitalWrite(ledPin, HIGH);
-            } else {
-                digitalWrite(ledPin, LOW);
-            }
-        }
+    
+    void onDisconnect(BLEServer* pServer) override {
+        deviceConnected = false;
+        Serial.println("Device disconnected");
+        // Restart advertising when disconnected
+        pServer->startAdvertising();
+        Serial.println("Started advertising again");
     }
 };
 
 void setup() {
     Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);
-
-    // Create the BLE Device
-    BLEDevice::init("ESP32 - Yo");
-
+    
+    // Initialize BLE device
+    BLEDevice::init(DEVICE_NAME);
+    
     // Create the BLE Server
     pServer = BLEDevice::createServer();
     pServer->setCallbacks(new MyServerCallbacks());
-
+    
     // Create the BLE Service
     BLEService *pService = pServer->createService(SERVICE_UUID);
-
-    // Create a BLE Characteristic
-    pSensorCharacteristic = pService->createCharacteristic(
-            SENSOR_CHARACTERISTIC_UUID,
-            BLECharacteristic::PROPERTY_READ   |
-            BLECharacteristic::PROPERTY_WRITE  |
-            BLECharacteristic::PROPERTY_NOTIFY |
-            BLECharacteristic::PROPERTY_INDICATE
-            );
-
-    // Create the ON button Characteristic
-    pLedCharacteristic = pService->createCharacteristic(
-            LED_CHARACTERISTIC_UUID,
-            BLECharacteristic::PROPERTY_WRITE
-            );
-
-    // Register the callback for the ON button characteristic
-    pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
-
-    // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-    // Create a BLE Descriptor
-    pSensorCharacteristic->addDescriptor(new BLE2902());
-    pLedCharacteristic->addDescriptor(new BLE2902());
-
+    
+    // Create BLE Characteristic
+    pCharacteristic = pService->createCharacteristic(
+        CHAR_UUID,
+        BLECharacteristic::PROPERTY_READ |
+        BLECharacteristic::PROPERTY_NOTIFY
+    );
+    
     // Start the service
     pService->start();
-
-    // Start advertising
+    
+    // Configure advertising
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(false);
-    pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMaxPreferred(0x12);
+    
+    // Set advertising parameters (optional but recommended)
+    pAdvertising->setMinInterval(0x20); // 0x20 * 0.625ms = 20ms
+    pAdvertising->setMaxInterval(0x40); // 0x40 * 0.625ms = 40ms
+    
     BLEDevice::startAdvertising();
-    Serial.println("Waiting a client connection to notify...");
+    Serial.println("BLE Device is advertising!");
 }
 
 void loop() {
-    // notify changed value
     if (deviceConnected) {
-        pSensorCharacteristic->setValue(String(value).c_str());
-        pSensorCharacteristic->notify();
-        value++;
-        Serial.print("New value notified: ");
-        Serial.println(value);
-        delay(3000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        Serial.println("Device disconnected.");
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("Start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-        Serial.println("Device Connected");
+        String message = String(latitude, 6) + "," + String(longitude, 6);
+        pCharacteristic->setValue(message.c_str());
+        pCharacteristic->notify();
+        delay(1000);
     }
 }
